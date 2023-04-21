e5d9942d49733666348167d48c542ac78d487d78
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-04-21 10:02:11 -0700

    added stuff

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,11 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types"; //import PropTypes
+import { v4 } from 'uuid'; // new code
 
-function NewTicketForm(props){
+function NewTicketForm(props) {
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <textarea
+          name='issue'
+          placeholder='Describe your issue.' />
+        <button type='submit'>Help!</button>
+      </form>
     </React.Fragment>
   );
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+
+    props.onNewTicketCreation({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: v4()
+    });
+  }
+
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import PropTypes from "prop-types";
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false,
+      mainTicketList: [] // new code
+    };
   }
 
-  render(){
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null; // new code
+    
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} /> 
+      buttonText = "Return to Ticket List";
+    }
+    
+    else {
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />; // new code
+      buttonText = "Add Ticket"; 
+    }
+    
     return (
       <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button> { /* new code */}
       </React.Fragment>
     );
   }
 
 }
+// Add propTypes for ticketList.
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketControl;
